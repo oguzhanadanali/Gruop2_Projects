f0869788706cf77d82ed2d2aa333f1fad1dc0274
Gürhan Ustali <gurhanustali@gmail.com>
2022-10-11 21:51:50 +0200

    Group2 Projects have been created.

---
+++ src/day07/MainApp.java
@@ -0,0 +1,13 @@
+package day07;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Merhaba");
+        System.out.println(5+8);
+        topla();
+    }
+
+    public static void topla(){
+        System.out.println(3+4);
+    }
+}
---
+++ src/day07/Variables.java
@@ -0,0 +1,15 @@
+package day07;
+
+public class Variables {
+    public static void main(String[] args) {
+        //= operatörü (assignment), sağında bulunan sabit bir değer (Literal) veya bir işlemin sonucunu
+        // solundaki değişkenin (Variable) temsil ettiği hafıza bölgesine kaydeder.
+        int yas=40; //bu satır bir java ifadesidir (statement)
+
+
+        //Karenin alanını ve çevresini hesaplayınız.
+        int kenar=5;
+        System.out.println(kenar*kenar);
+        System.out.println(4*kenar);
+    }
+}
