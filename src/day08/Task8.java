c32e4bebf387f61d349961b8cc8deebc4e1a39ce
Gürhan Ustali <gurhanustali@gmail.com>
2022-10-14 21:50:47 +0200

    Summaries

---
+++ src/day08/Task8.java
@@ -0,0 +1,11 @@
+package day08;
+
+public class Task8 {
+    public static void main(String[] args) {
+        int year=1491;
+        System.out.printf("IN %d COLUMBUS SAILED THE OCEANBLUE",year);
+        System.out.println();
+        year=1492;
+        System.out.printf("IN %d COLUMBUS SAILED THE OCEANBLUE",year);
+    }
+}
