904ab4308416fe332793c570f427e6bc2b051783
Gürhan Ustali <gurhanustali@gmail.com>
2022-10-19 20:11:45 +0200

    Relational Operators.

---
+++ src/day11/Orenkler2.java
@@ -0,0 +1,14 @@
+package day11;
+
+public class Orenkler2 {
+    public static void main(String[] args) {
+        int sayi=21;
+        int tahmin=18;
+
+        if (tahmin==sayi){
+            System.out.println("Bildiniz.");
+        }else {
+            System.out.println("Bilemediniz.");
+        }
+    }
+}
