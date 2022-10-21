a566d81849daa852a66b3eadecbe39d70628d07f
Gürhan Ustali <gurhanustali@gmail.com>
2022-10-21 22:00:50 +0200

    Third week overview.

---
+++ src/day13/Task.java
@@ -0,0 +1,12 @@
+package day13;
+
+public class Task {
+    public static void main(String[] args) {
+        /*
+            Klavyeden userName ve password bilgisi alınacaktır.
+            Eğer her iki veri doğru ise "Yönetim sayfasına hoşgeldiniz" mesajı verilecektir.
+            Eğer parola yanlış ise "Yanlış parola girdiniz. Lütfen tekrar deneyiniz." mesajı verilecektir.
+            Eğer kullanıcı adı yanlış girilmiş ise "Lütfen Sistem yöneticisi ile irtibat kurunuz" mesajı verilecektir.
+         */
+    }
+}
---
+++ src/day13/Tekrar5.java
@@ -0,0 +1,18 @@
+package day13;
+
+public class Tekrar5 {
+    public static void main(String[] args) {
+        //Şifrelemede xor kullanımı
+
+        int data=25;
+        int key=11;
+
+        int sifreliData=data^key;
+
+        System.out.println(sifreliData);
+
+        int cozulenData=18^25;
+        System.out.println(cozulenData);
+        //Simetrik ve asimetrik kripto
+    }
+}
