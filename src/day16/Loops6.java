5d71b8be2b83290fd6ad1d693bfdf301137397bd
Gürhan Ustali <gurhanustali@gmail.com>
2022-10-26 21:54:42 +0200

    CONTROL FLOW STATEMENTS FOR

---
+++ src/day16/Loops6.java
@@ -0,0 +1,20 @@
+package day16;
+
+public class Loops6 {
+    public static void main(String[] args) {
+        //TODO: 1'den 10'a (1 dahil, 10 dahil) kadar olan sayıların toplamını bulan
+        //java programını yazınız.
+
+
+        int toplam=0;
+
+        for (int i = 0; i <=10 ;) {
+            toplam=toplam+i;
+        }
+
+        System.out.println("Toplam = "+toplam);
+
+
+
+    }
+}
