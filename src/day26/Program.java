d791af923ef936d7e1e570173fc5e480413ef8c4
Gürhan Ustali <gurhanustali@gmail.com>
2022-11-10 21:35:57 +0100

    Principles of Object Oriented Programming

--- src/day26/Program.java
+++ src/day26/Program.java
@@ -2,11 +2,13 @@ package day26;
 
 import day26.mathe.DortIslem;
 
+
 public class Program {
     public static void main(String[] args) {
         DortIslem islem1=new DortIslem(5,7);
         //islem1.sayi1=5;
         //islem1.sayi2=7;
+
 
 
         System.out.println("Toplam = "+islem1.topla());
