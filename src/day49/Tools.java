fa36570f2da57a5bd3ebe03c20e459f18b999615
Gürhan Ustali <gurhanustali@gmail.com>
2022-12-14 21:35:06 +0100

    Collections 1

---
+++ src/day49/Tools.java
@@ -0,0 +1,5 @@
+package day49;
+
+public class Tools {
+    public static double PI_DEGERI=3.14;
+}
---
+++ src/day50/MainApp3.java
@@ -0,0 +1,55 @@
+package day50;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class MainApp3 {
+    public static void main(String[] args) {
+        Kitap k1=new Kitap(1,"Kitap 1");
+        //System.out.println(k1);
+        ArrayList<Kitap> kitapListesi=new ArrayList<>();
+        kitapListesi.add(new Kitap(5,"Kitap 5"));
+        kitapListesi.add(k1);
+
+        kitapListesi.add(0,new Kitap(3,"Kitap 3"));
+
+
+        k1=null;
+
+
+        //var k=new Kitap(3,"Kitap 3");
+
+        kitapListesi.forEach(k-> System.out.println(k));
+
+        Kitap k=new Kitap(7,"Kiatp 7");
+        var k8=new Kitap(8,"Kitap 8");
+
+        var x=5;
+        var isim="Ahmet";
+
+
+        /*
+        for(var k:kitapListesi){
+            System.out.println(k);
+        }
+
+         */
+
+
+        /*
+        Iterator<Kitap> iterator = kitapListesi.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+         */
+
+
+        /*
+        for (int i = 0; i < kitapListesi.size(); i++) {
+            System.out.println(kitapListesi.get(i));
+        }*/
+
+    }
+}
