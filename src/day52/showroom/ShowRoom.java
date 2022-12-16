29b12c9667fecb4b2820c8da84918ddac56e06d4
Gürhan Ustali <gurhanustali@gmail.com>
2022-12-16 21:33:23 +0100

    Review

---
+++ src/day52/showroom/ShowRoom.java
@@ -0,0 +1,74 @@
+package day52.showroom;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Iterator;
+import java.util.Objects;
+
+public class ShowRoom implements Iterable<Car> {
+    private static final int DEFAULT_CAPACITY=10;
+    private Car[] cars;
+    private int count;
+
+
+
+    public ShowRoom(int capacity){
+        cars=new Car[capacity];
+    }
+
+    public ShowRoom() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public int capacity(){
+        return cars.length;
+    }
+
+    public int size(){
+        return count;
+    }
+
+
+    public void add(Car car){
+        cars[count++]=car;
+        if(size()==capacity()/2+1){
+            Car[] tmp=new Car[size()];
+            for (int i = 0; i < size(); i++) {
+                tmp[i]=cars[i];
+            }
+            cars=new Car[capacity()*2];
+            for (int i = 0; i < size(); i++) {
+                cars[i]=tmp[i];
+            }
+        }
+    }
+
+    public Car get(int index){
+        Objects.checkIndex(index,size());
+        return cars[index];
+    }
+
+
+    @NotNull
+    @Override
+    public Iterator<Car> iterator() {
+        return new CarIterator();
+    }
+
+    private class CarIterator implements Iterator<Car>{
+        private int index;
+
+        @Override
+        public boolean hasNext() {
+            return index<size();
+        }
+
+        @Override
+        public Car next() {
+            Objects.checkIndex(index,size());
+            return cars[index++];
+        }
+    }
+
+
+}
