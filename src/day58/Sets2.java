88ff717eef095299495d4cf0be0303caf1c42d08
Gürhan Ustali <gurhanustali@gmail.com>
2022-12-26 21:14:25 +0100

    Stack, Queue and Set

---
+++ src/day58/Sets2.java
@@ -0,0 +1,17 @@
+package day58;
+
+import java.text.Collator;
+import java.util.Locale;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Sets2 {
+    public static void main(String[] args) {
+        Set<String> tset=new TreeSet<>(Collator.getInstance(new Locale("tr","TR")));
+        tset.add("Coşkun");
+        tset.add("Necati");
+        tset.add("Şahin");
+        tset.add("Çağla");
+        tset.forEach(item-> System.out.println(item));
+    }
+}
---
+++ src/day58/Sets3.java
@@ -0,0 +1,11 @@
+package day58;
+
+public class Sets3 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Book{
+
+}
